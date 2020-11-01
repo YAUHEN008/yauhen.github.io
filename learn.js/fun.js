dacc2b922827fc10c919e69a9298361c6f050109
Yauhen <72688877+YAUHEN008@users.noreply.github.com>
2020-11-01 12:11:37 +0200

    Add files via upload

---
+++ learn.js/fun.js
@@ -0,0 +1,43 @@
+// 1) 
+
+// let a 
+// let k=1
+// let y='lil'
+// let l=[]
+// let p=2>1
+// 2)
+// d=d+c
+// b=b+1
+// d=toostring;
+// 3)
+// var c= + "3"
+// var c= number("3");
+// let accessAllowed;
+// let age = prompt('How old are you?', '')
+// if (age > 18) {
+//     accessAllowed = true;
+
+// } else {
+//     accsessAllowed = false;
+
+// }
+// alert(accessAllowed);
+
+// 5)let i=0 
+// while(i<2){
+// alert (i); i++;
+// }
+// for (let i=o; i<3; i++)
+// alert(i);
+// 6)let mas=[1,2,3,4,5]
+// function mas(a,b)
+// {
+//     return a+b
+// }
+// for(let i=0; i<=mas.length;i++)
+// console.log(mas[i])
+// 7) 
+// let array =[1224,0,987,405,606]
+// array.max()
+// 8)mas.foreach(splice);
+9)
